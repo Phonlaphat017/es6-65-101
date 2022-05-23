921954c919307e6db9905667a0bfd04482be239b
Phonlapha Tabwongsri <phonlaphat9826@gmail.com>
2022-05-23 17:26:58 +0700

    first commit

---
+++ app1/app107.js
@@ -0,0 +1,23 @@
+let x = 5
+let y = 3;
+
+//Decrement
+console.log(`--y = ${--y}`)
+console.log(`y-- = ${y--}`)
+console.log(`y = ${y}`)
+
+//increment
+console.log(`++x = ${++x}`)
+console.log(`x++ = ${x++}`)
+
+//remainder
+console.log(`x % y = ${x % y}`)
+
+//Muliplication
+console.log(`x * y = ${x * y}`)
+
+//subtraction
+console.log(`x - y = ${x - y}`)
+
+//addition
+console.log(`x + y = ${x + y}`)
